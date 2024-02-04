746a65ba1864849d33d9aca8569c2f599b2ee386
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-02-04 07:44:56 +0100

    fix norminnete for file.c

--- ex00/file.c
+++ ex00/file.c
@@ -6,7 +6,7 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/03 15:20:35 by klamprak          #+#    #+#             */
-/*   Updated: 2024/02/04 06:31:21 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/02/04 07:44:14 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,70 +29,59 @@ int	is_space(char c)
 	return (!t);
 }
 
+// returns size of final dicts arrays
+int	read_char(char d_n[L][C], char d_w[L][C], char c_j_k[3], int is_n_spb_nl[3])
+{
+	if (c_j_k[0] == ':')
+	{
+		is_n_spb_nl[0] = 0;
+		is_n_spb_nl[1] = 1;
+		d_n[(int)c_j_k[1]][(int)c_j_k[2]] = '\0';
+		c_j_k[2] = 0;
+		return (0);
+	}
+	if (c_j_k[0] == '\n' && !is_n_spb_nl[0])
+	{
+		d_w[(int)c_j_k[1]++][(int)c_j_k[2]] = '\0';
+		c_j_k[2] = 0;
+		is_n_spb_nl[0] = 1;
+		is_n_spb_nl[2] = 1;
+		return (1);
+	}
+	if (is_space(c_j_k[0]) && is_n_spb_nl[1])
+		return (0);
+	else
+		is_n_spb_nl[1] = is_space(c_j_k[0]);
+	if (is_n_spb_nl[0])
+		d_n[(int)c_j_k[1]][(int)c_j_k[2]++] = c_j_k[0];
+	else
+		d_w[(int)c_j_k[1]][(int)c_j_k[2]++] = c_j_k[0];
+	return (0);
+}
+
 // return: -1 on error, 0 on success
 int	read_dict(char *file, char dict_num[L][C], char dict_words[L][C], int *size)
 {
-	int	fd;
+	int		fd;
 	ssize_t	bytes_read;
-	char	line[2];
-	int	k;
-	int	is_num_spacebef_nline[3];
-	int is_number;
-	int is_space_before;
-	int is_new_sentense;
+	char	c_j_k[3];
+	int		is_n_spb_nl[3];
 
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
 		return (-1);
-	*size = 0;
-	k = 0;
-	is_num_spacebef_nline[0] = 1;
-	is_num_spacebef_nline[1] = 1;
-	is_num_spacebef_nline[2] = 1;
-	is_number = 1;
-	is_space_before = 1;
-	is_new_sentense = 1;
-	bytes_read = read(fd, line, 1);
+	is_n_spb_nl[0] = 1;
+	is_n_spb_nl[1] = 1;
+	is_n_spb_nl[2] = 1;
+	bytes_read = read(fd, c_j_k, 1);
+	c_j_k[2] = 0;
 	while (bytes_read > 0)
 	{
-		if (line[0] == '\n' && is_new_sentense)
-		{
-			bytes_read = read(fd, line, 1);
-			continue;
-		}
-		else if (is_new_sentense && !is_space(line[0]))
-			is_new_sentense = 0;
-		if (line[0] == ':')
-		{
-			is_number = 0;
-			is_space_before = 1;
-			dict_num[*size][k] = '\0';
-			k = 0;
-			bytes_read = read(fd, line, 1);
-			continue;
-		}
-		if (line[0] == '\n' && !is_number)
-		{
-			dict_words[*size][k] = '\0';
-			(*size)++;
-			k = 0;
-			is_number = 1;
-			is_new_sentense = 1;
-			bytes_read = read(fd, line, 1);
-			continue;
-		}
-		if (is_space(line[0]) && is_space_before)
-		{
-			bytes_read = read(fd, line, 1);
-			continue;
-		}
-		else
-			is_space_before = is_space(line[0]);
-		if (is_number)
-			dict_num[*size][k++] = line[0];
-		else
-			dict_words[*size][k++] = line[0];
-		bytes_read = read(fd, line, 1);
+		if (is_n_spb_nl[2] && !is_space(c_j_k[0]) && c_j_k[0] != '\n')
+			is_n_spb_nl[2] = 0;
+		if (!(c_j_k[0] == '\n' && is_n_spb_nl[2]))
+			*size += read_char(dict_num, dict_words, c_j_k, is_n_spb_nl);
+		bytes_read = read(fd, c_j_k, 1);
 	}
 	if (bytes_read == -1)
 		return (-1);
@@ -100,28 +89,24 @@ int	read_dict(char *file, char dict_num[L][C], char dict_words[L][C], int *size)
 	return (1);
 }
 
-// int	read_char(char dict_num[L][C], char dict_words[L][C], int *size, int is_num_spacebef_nline[3])
+// int	main()
 // {
-// 	char	line[2];
-// 	int	k;
+// 	char	*filename;
+// 	char	dict_num[L][C];
+// 	char	dict_words[L][C];
+// 	int		size;
+// 	int		error;
+// 	int		i;
+
+// 	size = 0;
+// 	filename = "numbers.dict";
+// 	error = read_dict(filename, dict_num, dict_words, &size);
+// 	printf("-1 is error: %d\n", error);
+// 	i = 0;
+// 	while(i < size)
+// 	{
+// 		printf("%s--->%s\n", dict_num[i] ,dict_words[i]);
+// 		i++;
+// 	}
+// 	return (error);
 // }
-
-int main() {
-	char	*filename;
-	char	dict_num[L][C];
-	char	dict_words[L][C];
-	int		size;
-	int		error;
-	int		i;
-
-	filename = "numbers.dict";
-	error = read_dict(filename, dict_num, dict_words, &size);
-	printf("-1 is error: %d\n", error);
-	i = 0;
-	while(i < size)
-	{
-		printf("%s--->%s\n", dict_num[i] ,dict_words[i]);
-		i++;
-	}
-	return (error);
-}
